e1c1fc1cc0e0428007469968b166691e5882d8c2
Pedro Camara <pedro.camara@thoughtworks.com>
2022-10-11 12:22:00 -0300

    Starting the project

---
+++ src/test/java/com/javaprac/JavaPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
