f2b2b9b409cef523149340353a0dc2efe905f86b
Pedro Camara <57800238+pedrocmr@users.noreply.github.com>
2025-09-16 19:52:14 -0300

    teste tbd

--- src/main/java/com/javaprac/JavaPracApplication.java
+++ src/main/java/com/javaprac/JavaPracApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaPracApplication {
 
 	public static void main(String[] args) {
+		//teste tbd
 		SpringApplication.run(JavaPracApplication.class, args);
 	}
 
